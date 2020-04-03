3704023d6b5969e671a37433b562c570abcd6704
Sabrynna Farias <sabrynna_lima2000@hotmail.com>
2020-04-02 21:49:48 -0300

    Arma 1

---
+++ Assets/Sabrynna Shoots/Scripts/MultipleBallls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleBallls : MonoBehaviour
+{
+    public GameObject BigShoot;
+    public GameObject MultiplePrefab;
+    public Transform RoomSize;
+    Vector3 center;
+    Vector3 size;
+
+    public int Qtd = 100;
+
+    void Start()
+    {
+        center.x = RoomSize.transform.position.x;
+        center.y = RoomSize.transform.position.y;
+        center.z = RoomSize.transform.position.z;
+
+        size.x = RoomSize.transform.localScale.x;
+        size.z = RoomSize.transform.localScale.z;
+        size.y = RoomSize.transform.localScale.y;
+
+
+        Invoke("Destroy", 3f);
+    }
+    void Destroy()
+    {
+        BigShoot.SetActive(false);
+        Spawn();
+    }
+    void Spawn()
+    {
+
+        for (int i = 0; i <= Qtd; i++)
+        {
+            Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+
+            GameObject SpawnP = Instantiate(MultiplePrefab, pos, Quaternion.identity);
+        }
+
+    }
+}
---
+++ Assets/Sabrynna Shoots/Scripts/ShootScale.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScale : MonoBehaviour
+{
+    public Vector3 scaleChange;
+    public Transform fxPrefab;
+    public int countStop;
+    public int stopNumber;
+    public float frameRate;
+    void Start()
+    {
+        fxPrefab = GetComponent<Transform>();
+        InvokeRepeating("Scale", 1f, frameRate);
+    }
+    void Scale()
+    {
+        if (countStop <= stopNumber)
+        {
+            countStop++;
+            fxPrefab.transform.localScale += scaleChange;
+        }
+        else
+        {
+            CancelInvoke("Scale");
+        }
+    }
+}
