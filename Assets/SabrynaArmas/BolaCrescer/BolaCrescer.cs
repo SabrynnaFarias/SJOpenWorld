fe248362440e124fb2b8fc5fda408b8a2b6e1480
Sabrynna Farias <sabrynna_lima2000@hotmail.com>
2020-05-21 23:30:27 -0300

    Ajustes

---
+++ Assets/SabrynaArmas/BolaCrescer/BolaCrescer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolaCrescer : MonoBehaviour
+{
+    public float speed;
+    public float Repeat;
+    public int ScaleSizePerSecond;
+    public float timeToDestroy;
+    public GameObject BodyShot;
+
+    void Start()
+    {
+        
+        InvokeRepeating("Scale", 1, Repeat);
+        Invoke("Cancel", timeToDestroy);
+
+    }
+
+    void Update()
+    {
+        transform.position += Time.deltaTime * speed * transform.forward;
+    }
+
+    void Scale()
+    {
+        transform.localScale = new Vector3(transform.localScale.x + ScaleSizePerSecond, transform.localScale.y + ScaleSizePerSecond, transform.localScale.z + ScaleSizePerSecond);
+    }
+
+    void Cancel()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
