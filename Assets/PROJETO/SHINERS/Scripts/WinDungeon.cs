09a3b04e6cd28a569df7a77c6cdeb4360593c347
Jordan Savio <jordan.savio.95@gmail.com>
2020-06-19 23:49:05 -0300

    Dungeon 1 - Fix 7

--- Assets/PROJETO/SHINERS/Scripts/WinDungeon.cs
+++ Assets/PROJETO/SHINERS/Scripts/WinDungeon.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 public class WinDungeon : MonoBehaviour
 {
     public Transform SpawnReturn;
+    public GameObject Dungeon;
     Transform PlayerPosition;
+
+    public Animation Fade;
+
+    public GameObject Level;
+    public GameObject UILevel;
+    
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             PlayerPosition = other.gameObject.transform;
-            ReturnPlayer();
+            
+            Fade.Play();
+            Invoke("Cancel", 1);
+            Invoke("ReturnPlayer", 2);
+
         }
     }
 
+    void Cancel()
+    {
+        Dungeon.SetActive(false);
+    }
 
     void ReturnPlayer()
     {
         PlayerPosition.position = new Vector3(SpawnReturn.position.x, SpawnReturn.position.y, SpawnReturn.position.z);
+        Level.SetActive(true);
+        UILevel.SetActive(true);
+        
     }
 }
