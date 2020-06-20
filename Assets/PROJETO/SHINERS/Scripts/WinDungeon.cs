690a5cc839c742e24a9e8b9cbdac890aab7a773b
Jordan Savio <jordan.savio.95@gmail.com>
2020-06-20 00:03:37 -0300

    Dungeon 1 - Fix 9

--- Assets/PROJETO/SHINERS/Scripts/WinDungeon.cs
+++ Assets/PROJETO/SHINERS/Scripts/WinDungeon.cs
@@ -12,31 +12,29 @@ public class WinDungeon : MonoBehaviour
 
     public GameObject Level;
     public GameObject UILevel;
-    
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
             PlayerPosition = other.gameObject.transform;
-            
+
             Fade.Play();
             Invoke("Cancel", 1);
-            Invoke("ReturnPlayer", 2);
+
 
         }
     }
 
     void Cancel()
     {
-        Dungeon.SetActive(false);
-    }
-
-    void ReturnPlayer()
-    {
         PlayerPosition.position = new Vector3(SpawnReturn.position.x, SpawnReturn.position.y, SpawnReturn.position.z);
         Level.SetActive(true);
         UILevel.SetActive(true);
-        
+        Dungeon.SetActive(false);
     }
+
 }
+
+    
